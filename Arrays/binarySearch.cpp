e50fbd1df125dca17b8729cdb119de87505025ae
Sachin <rip260198@gmail.com>
2022-02-12 12:05:33 +0530

    binary search added

--- Arrays/binarySearch.cpp
+++ Arrays/binarySearch.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 using namespace std;
 
+// make starting pointer  and ending pointer
+// make while loop and find the mid point
+
+
  int binarySearch(int arr[],int n,int k) {
-           
+
+     int s=0, e=n;
+     int mid;
+     while(s<=e) {
+         mid=(s+e)/2;
+         if(arr[mid]==k){
+             return mid;
+         }
+         else if(arr[mid]>k) {
+             e=mid-1;
+
+         }
+         else{
+             s=mid+1;
+         }
+
+     }
+     return -1;    
  }
 
 int main()
