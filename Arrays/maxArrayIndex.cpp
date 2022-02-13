8f41765b45c7f672db2490944954b6cf34a8a06f
Sachin <rip260198@gmail.com>
2022-02-13 12:30:14 +0530

    new array file made

---
+++ Arrays/maxArrayIndex.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+// This program compare the array element and if suffix array is max remains its value and if
+// not then swap it.
+
+    
+int main() {
+    
+    #ifndef ONLINE_JUDGE // not a part of code used to beautify the code
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif   
+
+    int n, arr[n];
+    cin>>n;
+    for(int i=0;i<n;i++) {
+        cin>>arr[i];
+    } 
+     for(int i=0;i<n;i++){
+         if(arr[i+1]<arr[i]){
+             arr[i+1]=arr[i];
+         }
+         cout<<arr[i]<<" ";
+        
+     }
+    
+}
---
+++ Arrays/subArray.cpp
@@ -0,0 +1,6 @@
+// if u have the array ex 1,2,3,4;
+// then max subarray possible is (n*(n+1))/2 
+// so here the subarray which are possible is 
+// 1  1,2  1,2,3  1,2,3,4   2  2,3  2,3,4   3  3,4   4
+// therefore for n=4 the subarrays are 10
+
